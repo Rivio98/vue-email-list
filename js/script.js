84928daf48fbb66eeda5eae6261ee73bfcabe727
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-25 16:44:04 +0200

    [UPD] Added methods for add email generated by API and added style

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            generatedEmail: [],
         }
     },
+    created() {
+        this.generateTenEmails();
+    },
     methods: {
-
+        generateTenEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+                    this.generatedEmail.push(resp.data.response)
+                });
+            }
+        }
     }
-})
+}).mount('#app');
